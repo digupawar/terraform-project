71616def96c97b3c327a83ad56f3c52b5a110b99
digupawar <72307345+digupawar@users.noreply.github.com>
2024-06-17 20:38:49 +0530

    Update userdata1.sh

--- userdata1.sh
+++ userdata1.sh
@@ -3,7 +3,10 @@ apt update
 apt install -y apache2
 
 # Get the instance ID using the instance metadata
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+        && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id
+
+INSTANCE_ID=$TOKEN
 
 # Install the AWS CLI
 apt install -y awscli
